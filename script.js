92d9d060a61747b900027aa71167a4e63bc1419b
Eric Roeters <erroeters@gmail.com>
2022-08-05 14:27:03 -0400

    attempting to create GUI for game

--- script.js
+++ script.js
@@ -1,50 +1,76 @@
-function playRound(playerSelection) {
-    let compChoice = ["Rock", "Paper", "Scissors"];
-    let randomizer = Math.floor(Math.random()* 3);
-    let compGuess = compChoice[randomizer];
-    let playerGuess = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+// working console game
 
-    if (playerGuess == "Rock" || playerGuess == "Paper" || playerGuess == "Scissors") {
+// function playRound(playerSelection) {
+//     let compChoice = ["Rock", "Paper", "Scissors"];
+//     let randomizer = Math.floor(Math.random()* 3);
+//     let compGuess = compChoice[randomizer];
+//     let playerGuess = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
 
-        //computer guesses paper
+//     if (playerGuess == "Rock" || playerGuess == "Paper" || playerGuess == "Scissors") {
 
-        if (playerGuess == "Rock" && compGuess == "Paper") {
-            console.log(`The computer has chosen ${compGuess}`)
-            console.log("Paper covers rock, the computer wins!");
-        } else if (playerGuess == "Paper" && compGuess == "Paper") {
-            console.log(`The computer has chosen ${compGuess}`)
-            console.log("It's a tie! Try again.");
-        } else if (playerGuess == "Scissors" && compGuess == "Paper") {
-            console.log(`The computer has chosen ${compGuess}`)
-            console.log("Scissors cut paper, you win!");
-        }
+//         //computer guesses paper
 
-        //computer guesses rock
+//         if (playerGuess == "Rock" && compGuess == "Paper") {
+//             console.log(`The computer has chosen ${compGuess}`)
+//             console.log("Paper covers rock, the computer wins!");
+//         } else if (playerGuess == "Paper" && compGuess == "Paper") {
+//             console.log(`The computer has chosen ${compGuess}`)
+//             console.log("It's a tie! Try again.");
+//         } else if (playerGuess == "Scissors" && compGuess == "Paper") {
+//             console.log(`The computer has chosen ${compGuess}`)
+//             console.log("Scissors cut paper, you win!");
+//         }
 
-        else if (playerGuess == "Rock" && compGuess == "Rock") {
-            console.log(`The computer has chosen ${compGuess}`)
-            console.log("It's a tie! Try again.");
-        } else if (playerGuess == "Paper" && compGuess == "Rock") {
-            console.log(`The computer has chosen ${compGuess}`)
-            console.log("Paper covers rock, you win!");
-        } else if (playerGuess == "Scissors" && compGuess == "Rock") {
-            console.log(`The computer has chosen ${compGuess}`)
-            console.log("Rock smashes scissors, the computer wins!");
-        }
-        //computer guesses scissors
+//         //computer guesses rock
 
-        if (playerGuess == "Rock" && compGuess == "Scissors") {
-            console.log(`The computer has chosen ${compGuess}`)
-            console.log("Rock smashes scissors, you win!");
-        } else if (playerGuess == "Paper" && compGuess == "Scissors") {
-            console.log(`The computer has chosen ${compGuess}`)
-            console.log("Scissors cut paper, the computer wins!");
-        } else if (playerGuess == "Scissors" && compGuess == "Scissors") {
-            console.log(`The computer has chosen ${compGuess}`)
-            console.log("It's a tie! Try again.");
-        }
-    } else {
-        return "Please enter Rock, Paper, or Scissors";
-    }
+//         else if (playerGuess == "Rock" && compGuess == "Rock") {
+//             console.log(`The computer has chosen ${compGuess}`)
+//             console.log("It's a tie! Try again.");
+//         } else if (playerGuess == "Paper" && compGuess == "Rock") {
+//             console.log(`The computer has chosen ${compGuess}`)
+//             console.log("Paper covers rock, you win!");
+//         } else if (playerGuess == "Scissors" && compGuess == "Rock") {
+//             console.log(`The computer has chosen ${compGuess}`)
+//             console.log("Rock smashes scissors, the computer wins!");
+//         }
+//         //computer guesses scissors
+
+//         if (playerGuess == "Rock" && compGuess == "Scissors") {
+//             console.log(`The computer has chosen ${compGuess}`)
+//             console.log("Rock smashes scissors, you win!");
+//         } else if (playerGuess == "Paper" && compGuess == "Scissors") {
+//             console.log(`The computer has chosen ${compGuess}`)
+//             console.log("Scissors cut paper, the computer wins!");
+//         } else if (playerGuess == "Scissors" && compGuess == "Scissors") {
+//             console.log(`The computer has chosen ${compGuess}`)
+//             console.log("It's a tie! Try again.");
+//         }
+//     } else {
+//         return "Please enter Rock, Paper, or Scissors";
+//     }
+// }
+
+// let rock = document.getElementById("rock").value; 
+// let paper = document.getElementById("paper").value; 
+// let scissors = document.getElementById("scissors").value; 
+
+function computerGuess() {
+    const compChoice = ["Rock", "Paper", "Scissors"];
+    const randomizer = Math.floor(Math.random()* 3);
+    return compChoice[randomizer];
 }
 
+let playerChoice = document.querySelectorAll(".buttonClick");
+
+playerChoice.forEach(e1 => {
+    e1.addEventListener("click", output);
+});
+
+function output() {
+    console.log(this.value)
+    return this.value;
+}
+
+function game() {
+    
+}
